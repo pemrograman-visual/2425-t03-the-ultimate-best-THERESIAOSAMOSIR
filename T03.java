8096394169b0030f0bf8a3b8948d8c67dc643c84
THERESIAOSAMOSIR <samosirtheresia23@gmail.com>
2024-10-02 16:55:28 +0700

    pair 8

--- T03.java
+++ T03.java
@@ -1,1 +1,66 @@
-// NIM - Your Name
+// 12S24055 Theresia Oktaviani Samosir
+// 12S24016 Boy Harendy Simamora
+
+import java.util.*;
+import java.lang.Math;
+
+public class T03 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String isbn, judul, penulis, penerbit, formatbuku, kat, tub, diskon;
+        int stok, tahunterbit;
+        double margin, rating, hargabeli, katdiskon;
+
+        do {
+            isbn = input.nextLine();
+            if (isbn.equals("---")) {
+            } else {
+                judul = input.nextLine();
+                penulis = input.nextLine();
+                tahunterbit = Integer.parseInt(input.nextLine());
+                penerbit = input.nextLine();
+                formatbuku = input.nextLine();
+                hargabeli = Double.parseDouble(input.nextLine());
+                margin = Double.parseDouble(input.nextLine());
+                stok = Integer.parseInt(input.nextLine());
+                rating = Double.parseDouble(input.nextLine());
+                if (rating >= 4.7) {
+                    kat = "Best Pick";
+                } else {
+                    if (rating >= 4.5) {
+                        kat = "Must Read";
+                    } else {
+                        if (rating >= 4.0) {
+                            kat = "Recommended";
+                        } else {
+                            if (rating >= 3.0) {
+                                kat = "Average";
+                            } else {
+                                kat = "Low";
+                            }
+                        }
+                    }
+                }
+                katdiskon = margin / hargabeli * -1;
+                if (katdiskon >= 0.4) {
+                    diskon = "Once in a lifetime";
+                } else {
+                    if (katdiskon >= 0.2) {
+                        diskon = "Never come twice";
+                    } else {
+                        if (katdiskon > 0) {
+                            diskon = "No regret";
+                        }
+                    }
+                }
+                if (kat.equals("Best pick ") && diskon.equals("once in lifetime")) {
+                    tub = "The ultimate best";
+                } else {
+                    tub = "---";
+                }
+                System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbuku + "|" + hargabeli + "|" + margin + "|" + stok + "|" + rating + "|" + kat + "|" + diskon + "|" + tub);
+            }
+        } while (!isbn.equals("---"));
+    }
+}
